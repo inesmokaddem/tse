546bcff92ca22eefe0a33a635e0a0fc53aa0af94
Ines Mokaddem <ines.mokaddem@gmail.com>
2024-12-20 16:34:06 +0100

    🐛 TSE-02: debugg Card.tsx for children

--- src/components/reusable-ui/Header.tsx
+++ src/components/reusable-ui/Header.tsx
@@ -2,11 +2,11 @@ import styled from "styled-components"
 import { theme } from "@/theme/theme"
 import { PropsWithChildren } from "react"
 
-
-
-export default function Header({ children }: PropsWithChildren) {
+export default function Header({ children }: PropsWithChildren ) {
   return <HeaderStyled>{children}</HeaderStyled>
 }
+
+
 
 const HeaderStyled = styled.div`
   height: 70px;
