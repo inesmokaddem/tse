a24e7034387ab065cb2fb20837eb020e53c86964
Ines Mokaddem <ines.mokaddem@gmail.com>
2024-12-21 13:02:59 +0100

    ♻️ TSE-03: add type string in formatPrice in maths.ts

--- src/utils/maths.ts
+++ src/utils/maths.ts
@@ -1,4 +1,4 @@
-export function formatPrice(priceToFormat: number): string {
+export function formatPrice(priceToFormat: number | string): string {
   let price = priceToFormat
 
   // @TODO: perhaps change this to if(!price) return 0
